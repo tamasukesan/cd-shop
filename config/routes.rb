6272812fd39d90d0e969e107c672e9e43ef9be9c
tamasukesan <musser.3ao3ao@gmail.com>
2017-12-25 20:10:58 +0900

    更新

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-
   resources :items
   resources :orders, only:[:create,:show, :index]
   resources :order_items
